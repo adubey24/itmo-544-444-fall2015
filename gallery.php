2daf607cd2af22337ce1f1bd4abc39b23236ce1d
controller <adubey4@hawk.iit.edu>
2016-02-19 16:39:43 -0600

    added thumbnails

--- gallery.php
+++ gallery.php
@@ -1,55 +1,38 @@
 
-<Html>
-<head>
-<title>Gallery</title>
-</head>
-<body>
-
 <?php
 session_start();
-$email = $_POST["email"];
-echo $email;
-require 'vendor/autoload.php';
-
-$rds = new Aws\Rds\RdsClient([
-'version' => 'latest',
-'region'  => 'us-east-1'
-]);
-
-//Create Table
-$result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'ad-db',
-]);
-
-$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-
-    // Do something with the message
-    print "============\n". $endpoint . "================\n";
-   
-
-//echo "begin database";
-$link = mysqli_connect($endpoint,"controller","anvi2416","customerrecords") or die("Error " . mysqli_error($link));
-
-/* check connection */
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
-
-//below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
-
-$link->real_query("SELECT * FROM items WHERE email = '$email'");
-
-//$link->real_query("SELECT * FROM items");
-
-$res = $link->use_result();
-echo "Result set order...\n";
-while ($row = $res->fetch_assoc()) {
-    echo "<img src =\" " . $row['s3rawurl'] . "\" />";
-//echo <img src =\"" .$row['s3finishedurl'] . "\"/>";
-echo "<p>".$row['id'] . "Email: " . $row['email']."</p>";
-}
-$link->close();
+include 'header.php';
 ?>
-</body>
+	<!-- Foundation.js -->
+	<script>
+      		$(document).foundation();
+    	</script>
+
+	<!-- jQuery -->
+	<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+
+	<!-- Fotorama -->
+	<link href="resources/css/fotorama.css" rel="stylesheet">
+	<script src="resources/js/fotorama.js"></script>
+  
+<?php
+$email = $_SESSION["email"];
+echo "Entered email:  ".$email;
+require 'vendor/autoload.php';
+require 'resources/library/db.php';
+$link = getDbReadConn();
+$link->real_query("SELECT * FROM items WHERE email = '$email'");
+$res = $link->use_result();
+echo "<p/>";
+echo "Result set order:\n";
+echo "<div class=\"fotorama\" data-nav=\"thumbs\">";
+//echo "<div class="fotorama" data-nav="thumbs">";
+while ($row = $res->fetch_assoc()) {
+echo "<a href =\" " . $row['s3rawurl'] . "\" ><img src =\"" .$row['s3finishedurl'] . "\"/></a>";
+}
+echo "</div>";
+$link->close();
+//include 'footer.php';
+?>
+	</body>
 </html>
