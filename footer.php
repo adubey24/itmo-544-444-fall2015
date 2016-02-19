dba2376c58e125429e91e50926aedb1ce66345b3
controller <adubey4@hawk.iit.edu>
2016-02-19 15:47:42 -0600

    created footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+</p></p></p>
+	<script>
+      $(document).foundation();
+    </script>
+    <div class="fixed-bottom">
+    <nav class="top-bar bottom-bar" data-topbar role="navigation">
+        <section class="top-bar-section">
+                <!-- Right Nav Section -->
+                <ul class="center">
+                    <!--<a href="#">Contact</a>-->
+                </ul>
+        </section>
+    </nav>
+    </div>
+  </body>
+</html>
