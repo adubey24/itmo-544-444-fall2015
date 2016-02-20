4147055b1e03d02601c6751277e985ac4e425d09
controller <adubey4@hawk.iit.edu>
2016-02-19 20:11:27 -0600

    code cleanup

--- setup.php
+++ setup.php
@@ -3,86 +3,10 @@
 // Start the session^M
 
 require 'vendor/autoload.php';
+require 'resources/library/db.php';
 
-$rds = new Aws\Rds\RdsClient([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
+$link = getDbConn();
 
-$s3 = new Aws\s3\s3Client([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
-
-//Create table
-$result = $rds->createDBInstances([
-    'DBInstanceIdentifier' => 'ad-db',
-
-    #'AutoMinorVersionUpgrade' => true || false,
-    #'AvailabilityZone' => '<string>',
-    #'BackupRetentionPeriod' => <integer>,
-   # 'CharacterSetName' => '<string>',
-   # 'CopyTagsToSnapshot' => true || false,
-   # 'DBClusterIdentifier' => '<string>',
-
-   # 'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    #'DBInstanceIdentifier' => 'ad-db', // REQUIRED
-    #'DBName' => 'customerrecords',
-
-    #'DBParameterGroupName' => '<string>',
-    #'DBSecurityGroups' => ['<string>', ...],
-    #'DBSubnetGroupName' => '<string>',
-
-    #'Engine' => 'MySQL', // REQUIRED
-    #'EngineVersion' => '5.5.41',
-
-    #'Iops' => <integer>,
-    #'KmsKeyId' => '<string>',
-   # 'LicenseModel' => '<string>',
-  
-#'MasterUserPassword' => 'anvi2416',
- #   'MasterUsername' => 'controller',
-
-    #'MultiAZ' => true || false,
-    #'OptionGroupName' => '<string>',
-    #'Port' => <integer>,
-    #'PreferredBackupWindow' => '<string>',
-    #'PreferredMaintenanceWindow' => '<string>',
-    
-#'PubliclyAccessible' => true,
-
-    #'StorageEncrypted' => true || false,
-    #'StorageType' => '<string>',
-   # 'Tags' => [
-   #     [
-   #         'Key' => '<string>',
-   #         'Value' => '<string>',
-   #     ],
-        // ...
-   # ],
-    #'TdeCredentialArn' => '<string>',
-    #'TdeCredentialPassword' => '<string>',
-   # 'VpcSecurityGroupIds' => ['<string>', ...],
-
-]);
-
-#print "Create RDS DB results: \n";
-
-# print_r($rds);
-
-#$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'ad-db']);
-
-// Create a table 
-
-#$result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'ad-db'
-#]);
-
-$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
-print "============\n". $endpoint . "================\n";
-
-$link = mysqli_connect($endpoint,"controller","anvi2416","customerrecords") or die("Error " . mysqli_connect_error($link)); 
-#echo "Here is the result: " . $link;
 $sql_comments = "CREATE TABLE comments 
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -92,6 +16,7 @@ Content VARCHAR(500)
 )";
 
 $retVal1 = $link->query($sql_comments);
+
 if($retVal1 === TRUE) {
 print "Comments Table Created";
 } else {
@@ -110,13 +35,30 @@ filename Varchar(256),
 status TinyInt(3), 
 issubscribed TinyInt(3)
 )";
+
 $retVal2 = $link->query($sql_items);
+
 if($retVal2 === TRUE) {
 print "Items Table Created";
 } else {
 print "Could not create Items table";
 }
 
+$sql_config = "CREATE TABLE cloud_gallery_config 
+(
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+config_key Varchar(20),
+config_value Varchar(80)
+)";
+
+$retVal3 = $link->query($sql_config);
+
+if($retVal3 === TRUE) {
+print "cloud_gallery_config Table Created";
+} else {
+print "Could not create cloud_gallery_config table";
+}
+
 $link->close();
-print "\n"
+print "\n";
 ?>
